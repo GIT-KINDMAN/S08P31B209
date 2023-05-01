9c753ebadae37134df5a984e08e7945147c7e293
한재욱 <hju9707@naver.com>
2023-05-01 15:25:33 +0900

    fix: HomeHeader error fix #6

--- Front/src/pages/test/index.tsx
+++ Front/src/pages/test/index.tsx
@@ -1,11 +1,9 @@
 import "twin.macro";
-import HomeHeader from "@/components/organisms/homeHeader";
 import { Button } from "@/components/atoms";
 
 const Test = () => {
   return (
     <>
-      <HomeHeader label="Test Page" />
       <main>
         <Button icon="book" label="book button"></Button>
       </main>
