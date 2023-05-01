8d4ee8896e8db887e0de0044850a426f9c253ebe
hunsang <gjstkd123@naver.com>
2023-05-01 10:06:04 +0900

    fix:GlobalStyles 오류 수정

--- Front/src/styles/GlobalStyles.tsx
+++ Front/src/styles/GlobalStyles.tsx
@@ -3,7 +3,6 @@ import tw, { theme, GlobalStyles as BaseStyles } from "twin.macro";
 
 const CustomStyles = createGlobalStyle({
   body: {
-    WebkitTapHighlightColor: theme`colors.purple.500`,
     ...tw`antialiased`,
   },
 });
