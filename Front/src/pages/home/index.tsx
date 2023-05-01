5394bdcf95068091e8383627f55ceafb6d2984a3
한재욱 <hju9707@naver.com>
2023-05-01 16:06:58 +0900

    fix: HomeHeader 제거 #6

--- Front/src/pages/home/index.tsx
+++ Front/src/pages/home/index.tsx
@@ -1,10 +1,8 @@
 import "twin.macro";
-import HomeHeader from "@/components/organisms/homeHeader";
 
 const Home = () => {
   return (
     <>
-      <HomeHeader label="Home Page" />
       <main> main </main>
       <footer>Footer</footer>
     </>
