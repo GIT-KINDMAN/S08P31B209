a171b979d3f9ab0d165f4943285afca8818b59f2
hunsang <gjstkd123@naver.com>
2023-05-01 15:04:45 +0900

    feat:textarea text, password, edit 추가

--- Front/src/pages/test/index.tsx
+++ Front/src/pages/test/index.tsx
@@ -1,14 +1,18 @@
 import "twin.macro";
-import HomeHeader from "@/components/organisms/homeHeader";
-import { Button } from "@/components/atoms";
+// import HomeHeader from "@/components/organisms/homeHeader";
+import { Button, TextArea } from "@/components/atoms";
 
 const Test = () => {
   return (
     <>
-      <HomeHeader label="Test Page" />
+      {/* <HomeHeader label="Test Page" /> */}
       <main>
         <Button icon="book" label="book button"></Button>
+        <TextArea variant="text" />
+        <TextArea variant="password" />
+        <TextArea variant="edit" />
       </main>
+
       <footer>Footer</footer>
     </>
   );
