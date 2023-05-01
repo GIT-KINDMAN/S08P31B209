83ffe80d3d9ec67cefde4215365fb9a43ba06269
hunsang <gjstkd123@naver.com>
2023-05-01 17:47:15 +0900

    메인페이지 일부 구성

--- Front/src/pages/home/index.tsx
+++ Front/src/pages/home/index.tsx
@@ -7,12 +7,12 @@ const Home = () => {
   const navigate = useNavigate();
   return (
     <>
-
       {/* <HomeHeader label="Home Page" /> */}
 
       <main> main </main>
       <footer>Footer</footer>
-      <div onClick={() => navigate("/test")}>테스트 페이지로</div>
+      <div onClick={() => navigate("/test")}>테스트 페이지로 이동</div>
+      <div onClick={() => navigate("/main")}>메인 페이지로 이동</div>
     </>
   );
 };
--- Front/src/pages/index.tsx
+++ Front/src/pages/index.tsx
@@ -4,3 +4,4 @@ export { default as DocsBox } from "./docsBox";
 export { default as ErrorPage } from "./error/errorPage";
 
 export { default as Test } from "./test";
+export { default as MainPage } from "./main";
