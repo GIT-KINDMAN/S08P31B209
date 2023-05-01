0a2f823329a480a742565b20d4f4eaafad56a9f0
한재욱 <hju9707@naver.com>
2023-05-02 08:57:00 +0900

    feat: Radio Label 추가 #8

--- Front/src/components/atoms/Radio/Radio.tsx
+++ Front/src/components/atoms/Radio/Radio.tsx
@@ -1,7 +1,12 @@
 import StyledRadio from "./Radio.styled";
 import { InputProps } from "./Radio.types";
 
-const Radio = ({ checked, onChange }: InputProps) => {
-  return <StyledRadio checked={checked} onChange={onChange}></StyledRadio>;
+const Radio = ({ label, checked, onChange }: InputProps) => {
+  return (
+    <div>
+      <StyledRadio checked={checked} onChange={onChange}></StyledRadio>
+      <span>{label}</span>
+    </div>
+  );
 };
 export default Radio;
