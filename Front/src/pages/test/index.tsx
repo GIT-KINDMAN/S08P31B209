150952b75a79383c18ee0a4ccd36ce9afdc568d5
한재욱 <hju9707@naver.com>
2023-05-02 14:07:40 +0900

    feat: TableViewHeader test page #19

--- Front/src/pages/test/index.tsx
+++ Front/src/pages/test/index.tsx
@@ -1,12 +1,16 @@
 // import HomeHeader from "@/components/organisms/homeHeader";
-
 import { Button, Radio, TableHeader, TextArea } from "@/components/atoms";
+import { TableViewHeader } from "@/components/molecules";
 
 import { useState } from "react";
 import "twin.macro";
 
 const Test = () => {
   const [checked, setChecked] = useState(false);
+
+  const handleTableHeaderChange = (isChecked: boolean) => {
+    setChecked((prevChecked) => !prevChecked);
+  };
 
   const handleRadioChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
     setChecked(e.target.checked);
@@ -16,6 +20,11 @@ const Test = () => {
       {/* <HomeHeader label="Test Page" /> */}
 
       <main>
+        <TableViewHeader
+          checked={checked}
+          icon="check"
+          onChange={handleTableHeaderChange}
+        ></TableViewHeader>
         <Button icon="book" label="book button"></Button>
         <TextArea variant="number" />
         <TextArea variant="password" />
