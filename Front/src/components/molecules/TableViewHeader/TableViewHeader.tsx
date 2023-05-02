aac5fcf16f9e974ddc6aa72aab892b842a899ea6
한재욱 <hju9707@naver.com>
2023-05-02 15:45:03 +0900

    feat: test page fix #20

---
+++ Front/src/components/molecules/TableViewHeader/TableViewHeader.tsx
@@ -0,0 +1,14 @@
+import { CheckBox } from "@/components/atoms";
+import { InputProps } from "@/components/atoms/CheckBox/CheckBox.types";
+
+import { styled } from "twin.macro";
+
+export const TableViewHeader = ({ checked, icon, onChange }: InputProps) => {
+  return (
+    <div tw="pl-2 h-9 flex items-center">
+      <CheckBox checked={checked} icon={icon} onChange={onChange}></CheckBox>
+    </div>
+  );
+};
+
+export default TableViewHeader;
