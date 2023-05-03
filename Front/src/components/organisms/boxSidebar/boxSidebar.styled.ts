c07a39ab3c27465b5a5d55e89e9b9fe72bdf502a
권동규 <ratsae0117@gmail.com>
2023-05-03 10:25:27 +0900

    feat: organ boxsidebar 추가 #21

---
+++ Front/src/components/organisms/boxSidebar/boxSidebar.styled.ts
@@ -0,0 +1,9 @@
+import { StyleProps } from "./boxSidebar.types";
+
+import tw, { styled } from "twin.macro";
+
+export const StyledBoxSidebar = styled.div(({ variable }: StyleProps) => [
+  tw`flex flex-col`,
+]);
+
+export default StyledBoxSidebar;
