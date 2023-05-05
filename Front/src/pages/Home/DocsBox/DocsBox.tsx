a7ba02fa8d358c73efca62620d35f34a599be95b
한재욱 <hju9707@naver.com>
2023-05-05 17:55:44 +0900

    feat: TableHeader 영역 divide 및 size props 추가 #44

--- Front/src/pages/Home/DocsBox/DocsBox.tsx
+++ Front/src/pages/Home/DocsBox/DocsBox.tsx
@@ -1,10 +1,27 @@
-import { redirect } from "react-router-dom";
+import { TableHeader } from "@/components/atoms";
+import { TableViewHeader, TableViewRow } from "@/components/molecules";
+
 import "twin.macro";
 
 const DocsBox = () => {
   return (
     <>
-      <div tw="border bg-orange-100 w-full h-full">DocsBox</div>
+      <div tw="border bg-orange-100 w-full h-full">
+        <div tw="flex bg-lightgray-700 h-[3.5rem] items-center ml-[8rem] mr-[8rem]">
+          <TableHeader
+            label="내 보관함"
+            width="40rem"
+            height="3.5rem"
+            tw="bg-orange-400"
+            size="lg"
+          ></TableHeader>
+        </div>
+        <div tw="flex flex-col h-full">
+          <div tw="grow bg-orange-300">SearchBar</div>
+          <div tw="grow bg-orange-400">TableViewHeader</div>
+          <div tw="grow bg-orange-500">TableViewRow</div>
+        </div>
+      </div>
     </>
   );
 };
