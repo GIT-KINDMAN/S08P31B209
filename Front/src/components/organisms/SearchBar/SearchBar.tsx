fa1255fe0d7071e13fcf86da088905a08ddc1ea4
한재욱 <hju9707@naver.com>
2023-05-06 15:48:53 +0900

    feat: docs box 기본틀 제작 #44

--- Front/src/components/organisms/SearchBar/SearchBar.tsx
+++ Front/src/components/organisms/SearchBar/SearchBar.tsx
@@ -5,7 +5,7 @@ import tw from "twin.macro";
 const SearchBar = () => {
   return (
     <>
-      <div tw="flex flex-row items-center space-x-1 border-lightgray-700 border-b-2">
+      <div tw="flex flex-row items-center space-x-1 border-lightgray-700 border-b-2 h-[3rem] mt-[0.5rem]">
         <TextArea variant="search"></TextArea>
         <Icon icon="search" iconColor={tw`text-blue-800`} size="lg"></Icon>
       </div>
--- Front/src/pages/Home/DocsBox/DocsBox.tsx
+++ Front/src/pages/Home/DocsBox/DocsBox.tsx
@@ -1,10 +1,12 @@
-import { TableHeader } from "@/components/atoms";
+import { Icon, TableHeader } from "@/components/atoms";
 import { TableViewHeader, TableViewRow } from "@/components/molecules";
 import { PathSideToolBar, SearchBar } from "@/components/organisms";
 
 import "twin.macro";
 
 const DocsBox = () => {
+  // const tableData = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+  const tableData: string[] = [];
   return (
     <>
       <div tw="border bg-orange-100 w-full" className="header">
@@ -17,16 +19,42 @@ const DocsBox = () => {
             size="lg"
           ></TableHeader>
         </div>
-        <div tw="flex flex-col" className="contents">
-          <div tw="flex grow bg-orange-300 justify-end h-[3rem] mr-[3rem]">
+        <div tw="flex flex-col h-full" className="contents">
+          <div tw="flex  bg-orange-300 justify-end h-[5rem] mr-[3rem]">
             <SearchBar />
           </div>
-          <div tw="flex grow bg-red-500">
+          <div tw="flex  bg-red-500 justify-end m-[0.5rem] mr-[3rem]">
             <PathSideToolBar />
           </div>
-          <div className="table">
-            <div tw="grow bg-orange-400">TableViewHeader</div>
-            <div tw="grow bg-orange-500">TableViewRow</div>
+          <div className="table" tw="flex grow flex-col items-center">
+            <div tw="bg-orange-400">
+              <TableViewHeader
+                isBookmarkActive={false}
+                labels={[
+                  "문서 이름",
+                  "상태",
+                  "문서 수정일",
+                  "문서 공유일",
+                  "문서 마감일",
+                  "더보기",
+                ]}
+              ></TableViewHeader>
+            </div>
+            <div tw="flex bg-orange-500 w-4/5 h-4/5 justify-center">
+              {tableData.length > 0 ? (
+                tableData.map((data, index) => (
+                  <TableViewRow
+                    key={index}
+                    isBookmarkActive={true}
+                  ></TableViewRow>
+                ))
+              ) : (
+                <div tw="flex flex-col justify-center">
+                  <Icon icon="broom" iconType="rr" size="5xl"></Icon>
+                  <div tw="text-3xl m-1">문서함이 비어있어요</div>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
