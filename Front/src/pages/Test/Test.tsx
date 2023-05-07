19bcccd2a37c96ac971b2906118214e3e715d14f
juseong <kjschocolate@gmail.com>
2023-05-07 10:14:02 +0900

    feat: TableHeader, Button, Carousel Test 추가

--- Front/src/pages/Test/Test.tsx
+++ Front/src/pages/Test/Test.tsx
@@ -1,10 +1,34 @@
-import { TableHeader } from "@/components/atoms";
+import { Button, Carousel, TableHeader } from "@/components/atoms";
 import { TableViewHeader, TableViewRow } from "@/components/molecules";
 import { EmptyDocs, PathSideToolBar, SearchBar } from "@/components/organisms";
+
+import LogoImg from "../../assets/DocDoc.png";
 
 import tw, { styled } from "twin.macro";
 
 const Wrapper = styled.div(tw`border border-black flex justify-center`);
+
+// const tableData: string[] = [
+//   "1",
+//   "2",
+//   "3",
+//   "4",
+//   "5",
+//   "6",
+//   "7",
+//   "8",
+//   "9",
+//   "10",
+// ]; // 시험용 임시데이터
+const tableData: string[] = [];
+const lables: string[] = [
+  "문서 이름",
+  "상태",
+  "문서 수정일",
+  "문서 공유일",
+  "문서 마감일",
+  "더보기",
+];
 
 const Test = () => {
   return (
@@ -13,8 +37,40 @@ const Test = () => {
         <div>
           <h1>Atoms</h1>
           <Wrapper>
-            TableHeader
-            <TableHeader />
+            <b>TableHeader</b>
+            <div>
+              {" "}
+              <TableHeader label="내 보관함" height="3.5rem" size="2xl" />
+            </div>
+          </Wrapper>
+          <Wrapper>
+            <b>Button</b>
+            <div>
+              <Button
+                label="button"
+                variant="primary"
+                icon="test"
+                size="small"
+              />
+              <Button
+                label="button"
+                variant="secondary"
+                icon="test"
+                size="small"
+              />
+              <Button
+                label="button"
+                variant="mainnav"
+                icon="test"
+                size="small"
+              />
+            </div>
+          </Wrapper>
+          <Wrapper>
+            <b>Carousel</b>
+            <div>
+              <Carousel image={LogoImg} />
+            </div>
           </Wrapper>
         </div>
       </Wrapper>
