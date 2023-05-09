bacd5bcfd18e9279a70696693832abc4fc3847c9
juseong <kjschocolate@gmail.com>
2023-05-09 16:17:03 +0900

    fix: 폴더 구조 변경

---
+++ server/src/main/java/b209/docdoc/server/exception/DocdocServerException.java
@@ -0,0 +1,14 @@
+package b209.docdoc.server.exception;//package b209.docdoc.server.exception;
+
+import lombok.Getter;
+
+@Getter
+public class DocdocServerException extends RuntimeException {
+    private ErrorCode errorCode;
+
+    public DocdocServerException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+
+
+}
