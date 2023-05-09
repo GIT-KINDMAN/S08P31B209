b310e0f54284a2acebbca617a7a63671e5db501d
권동규 <ratsae0117@gmail.com>
2023-05-09 13:20:19 +0900

    feat: homeSidebar 카테고리 추가 #52

--- Front/src/pages/Home/Home.tsx
+++ Front/src/pages/Home/Home.tsx
@@ -15,7 +15,7 @@ const Home = () => {
         <div tw="flex flex-row ">
           <div
             className="HomeSidebar"
-            tw="flex flex-col justify-between max-w-[16rem] min-h-screen bg-blue-900"
+            tw="flex flex-col justify-between min-w-[16rem] max-w-[16rem] min-h-screen max-h-fit bg-blue-900"
           >
             <div className="SideHeader">
               <div
@@ -49,25 +49,125 @@ const Home = () => {
                   </label>
                 </div>
               </div>
-              <div className="ProfileView" tw="flex px-4 pt-2 pb-4">
+              <div className="CreateTemplate" tw="flex px-4 pt-2 pb-4">
                 <button
-                  className="LoginBtn"
+                  className="CreateTemplateBtn"
                   tw="p-2 w-full rounded-[0.5rem] bg-orange-600 font-bold"
                   onClick={() => navigate("/editor")}
                 >
                   생성하기
                 </button>
               </div>
-              <div className="CategoryMenu" tw="grow bg-blue-800">
-                CategoryMenu
+              <div className="CategoryMenu" tw="px-2 py-1 grow text-white">
+                <ul
+                  className="CategoryMenuList"
+                  tw="px-4 pt-2 pb-10 rounded-[0.5rem] bg-blue-800"
+                >
+                  <li className="CategoryMenuListItem">
+                    <div
+                      tw="w-fit pt-1 pb-2 hocus:(text-orange-600) cursor-pointer"
+                      onClick={() => navigate("/home/recive")}
+                    >
+                      <i
+                        className="fi fi-rr-folder"
+                        tw="pr-1 cursor-pointer"
+                      ></i>
+                      <label tw="cursor-pointer">받은 문서함</label>
+                    </div>
+                  </li>
+                  <li className="CategoryMenuListItem">
+                    <div
+                      tw="w-fit pt-1 pb-2 hocus:(text-orange-600) cursor-pointer"
+                      onClick={() => navigate("/home/send")}
+                    >
+                      <i
+                        className="fi fi-rr-folder"
+                        tw="pr-1 cursor-pointer"
+                      ></i>
+                      <label tw="cursor-pointer">보낸 문서함</label>
+                    </div>
+                  </li>
+                  <li className="CategoryMenuListItem">
+                    <div
+                      tw="w-fit pt-1 pb-2 hocus:(text-orange-600) cursor-pointer"
+                      onClick={() => navigate("/home/bookmark")}
+                    >
+                      <i className="fi fi-rr-star" tw="pr-1 cursor-pointer"></i>
+                      <label tw="cursor-pointer">중요 항목</label>
+                    </div>
+                  </li>
+                  <li className="CategoryMenuListItem">
+                    <div
+                      tw="w-fit pt-1 pb-2 hocus:(text-orange-600) cursor-pointer"
+                      onClick={() => navigate("/home/template")}
+                    >
+                      <i
+                        className="fi fi-rr-briefcase"
+                        tw="pr-1 cursor-pointer"
+                      ></i>
+                      <label tw="cursor-pointer">템플릿 관리</label>
+                    </div>
+                  </li>
+                  <li className="CategoryMenuListItem">
+                    <div
+                      tw="w-fit pt-1 pb-2 hocus:(text-orange-600) cursor-pointer"
+                      onClick={() => navigate("/home/trash")}
+                    >
+                      <i
+                        className="fi fi-rr-trash"
+                        tw="pr-1 cursor-pointer"
+                      ></i>
+                      <label tw="cursor-pointer">휴지통</label>
+                    </div>
+                  </li>
+                </ul>
               </div>
-              <div className="SettingMenu" tw="">
-                SettingMenu
+              <div className="SettingMenu" tw="px-4 py-2 text-white">
+                <ul className="SettingMenuList">
+                  <li className="SettingMenuListItem">
+                    <div
+                      tw="w-fit pt-1 pb-2 hocus:(text-orange-600) cursor-pointer"
+                      onClick={() => navigate("/setting")}
+                    >
+                      <i
+                        className="fi fi-sr-settings"
+                        tw="pr-1 cursor-pointer"
+                      ></i>
+                      <label tw="cursor-pointer">설정</label>
+                    </div>
+                  </li>
+                  <li className="SettingMenuListItem">
+                    <div
+                      tw="w-fit pt-1 pb-2 hocus:(text-orange-600) cursor-pointer"
+                      onClick={() => navigate("/notice")}
+                    >
+                      <i
+                        className="fi fi-sr-settings"
+                        tw="pr-1 hidden cursor-pointer"
+                      ></i>
+                      <label tw="cursor-pointer">공지사항</label>
+                    </div>
+                  </li>
+                  <li className="SettingMenuListItem">
+                    <div
+                      tw="w-fit pt-1 pb-2 hocus:(text-orange-600) cursor-pointer"
+                      onClick={() => navigate("/logout")}
+                    >
+                      <i
+                        className="fi fi-sr-settings"
+                        tw="pr-1 hidden cursor-pointer"
+                      ></i>
+                      <label tw="font-bold text-red-600 cursor-pointer">
+                        로그아웃
+                      </label>
+                    </div>
+                  </li>
+                </ul>
               </div>
             </div>
             <div
               className="SideFooter"
-              tw="flex flex-col justify-around min-h-[8rem]"
+              tw="flex flex-col justify-around min-h-[8rem] bg-lightgray-900"
             >
               <label tw="text-xs text-white pl-4">{footerCSInfo}</label>
               <div
