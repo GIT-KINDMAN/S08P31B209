9166acd4f7ecc3d440a137036bdfa54912ca9d3d
한재욱 <hju9707@gmail.com>
2023-05-11 06:49:54 +0900

    feat: template save api 구현 및 수정  #71

--- server/src/main/java/b209/docdoc/server/template/service/Impl/TemplateServiceImpl.java
+++ server/src/main/java/b209/docdoc/server/template/service/Impl/TemplateServiceImpl.java
@@ -1,27 +1,21 @@
 package b209.docdoc.server.template.service.Impl;
 
+import b209.docdoc.server.config.utils.UUIDGenerator;
 import b209.docdoc.server.email.service.EmailService;
-import b209.docdoc.server.entity.Member;
-import b209.docdoc.server.entity.Template;
-import b209.docdoc.server.entity.Templatefile;
-import b209.docdoc.server.entity.Widget;
+import b209.docdoc.server.entity.*;
 import b209.docdoc.server.exception.ErrorCode;
 import b209.docdoc.server.exception.MemberNotFoundException;
 import b209.docdoc.server.exception.SaveFileException;
 import b209.docdoc.server.exception.TemplateNotFoundException;
-import b209.docdoc.server.repository.MemberRepository;
-import b209.docdoc.server.repository.TemplateFileRepository;
-import b209.docdoc.server.repository.TemplateRepository;
-import b209.docdoc.server.repository.WidgetRepository;
+import b209.docdoc.server.repository.*;
 import b209.docdoc.server.template.dto.Request.DocumentTemplateSaveReqDTO;
-<<<<<<< HEAD
-import b209.docdoc.server.template.dto.Request.TemplateWidgetDTO;
+
 import b209.docdoc.server.template.dto.Response.TemplateNameResDTO;
-=======
+
 import b209.docdoc.server.template.dto.Response.TemplateResDTO;
 import b209.docdoc.server.template.dto.Response.TemplatefileResDTO;
 import b209.docdoc.server.template.dto.Response.WidgetResDTO;
->>>>>>> 80507c7015dbc8966053e15a2b6abf01c043cf54
+
 import b209.docdoc.server.template.service.TemplateService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -50,6 +44,8 @@ public class TemplateServiceImpl implements TemplateService {
     private final WidgetRepository widgetRepository;
     private final EmailService emailService;
 
+    private final ReceiverRepository receiverRepository;
+
     @Value("${file.upload-dir}") // application.properties에 설정된 파일 업로드 디렉터리 경로
     private String uploadDir;
 
@@ -68,6 +64,7 @@ public class TemplateServiceImpl implements TemplateService {
             memberDir.mkdirs();
         }
 
+        //1. 파일 저장
         try {
             pdfFile.transferTo(new File(memberDir, fileName));
         } catch (IOException e) {
@@ -85,56 +82,74 @@ public class TemplateServiceImpl implements TemplateService {
         } else {
             templateType = "UNKNOWN";
         }
+
+        //2. 문서파일 위치와 원본이름 DB 저장
+        Templatefile templatefile = Templatefile.builder().
+                templatefileOriginalName(fileName).
+                templatefileSavedName(fromEmail + "/" + fileName).
+                build();
+        templateFileRepository.save(templatefile);
+
+        String uuid = UUIDGenerator.generateUUID(); // 템플릿 uuid
+        // 4. 사용자 위젯 생성 및 저장
+        List<WidgetResDTO> templateWidget = documentTemplateSaveReqDTO.getWidgetResDTO();
+
+        String templateDeadline = documentTemplateSaveReqDTO.getTemplateDeadline(); // 템플릿 마감일
+        String templateName = documentTemplateSaveReqDTO.getTemplateName(); // 템플릿 이름
+
+        // 3. 사용자 템플릿 생성 db 저장
+        Template template = Template.builder().
+                member(member)
+                .templatefile(templatefile) // 템플릿 파일
+                .templateUuid(uuid) // 템플릿 uuid
+                .templateName(templateName) // 템플릿 파일 이름
+                .templateIsDeleted(false) // 삭제 안함
+                .templateDeadline(templateDeadline) // 마감일
+                .build();
+        templateRepository.save(template);
+
+        List<Widget> templateWidgets = templateWidget.stream().map(widget ->
+                        Widget.builder()
+                                .templateIdx(template.getTemplateIdx()) // 템플릿 idx
+                                .widgetName(widget.getWidgetType()) //  위젯 종류
+                                .widgetSx(widget.getWidgetSx()) // 왼쪽위 x 좌표
+                                .widgetDx(widget.getWidgetDx()) // 가로 길이
+                                .widgetSy(widget.getWidgetSy()) // 왼쪽위 y 좌표
+                                .widgetDy(widget.getWidgetDy()) // 세로 길이
+                                .widgetType(widget.getWidgetType()) //  위젯 종류
+                                .widgetContent(widget.getWidgetContent()) // 위젯 내용
+                                .widgetIsBase(false) // 기본 위젯 여부
+                                .widgetIsDeleted(false) // 삭제 여부
+                                .build())
+                .collect(Collectors.toList());
+        widgetRepository.saveAll(templateWidgets);
+
+        // 5. 수신자 이메일 전송 및 Receiver DB저장
+        for (int i = 0; i < documentTemplateSaveReqDTO.getTemplateName().length(); i++) { // 수신자들에게 템플릿 전송
+            String toName = documentTemplateSaveReqDTO.getToName().get(i); // 수신자 이름
+            String toEmail = documentTemplateSaveReqDTO.getToName().get(i); // 수신자 이메일
+
+            //수신자가 회원가입한 멤버인지 확인
+            boolean isMemmber = memberRepository.findByMemberEmail(toEmail).isPresent();
+
+            Receiver receiver = Receiver.builder()
+                    .template(template)
+                    .templatefile(templatefile)
+                    .receiverDocsName(documentTemplateSaveReqDTO.getTemplateName())
+                    .receiverDeadline(templateDeadline)
+                    .receiverIsMember(isMemmber) //수신자가 멤버 인지 확인
+                    .receiverIsCompeleted(false)// 문서 작성 완료 여부
+                    .receiverSenderName(member.getMemberName())//발신자 이름
+                    .receiverSenderEmail(fromEmail)//발신자 이메일
+                    .receiverIsDeleted(false)
+                    .receiverEmail(toEmail)//수신자 이메일
+                    .build();
+
+            receiverRepository.save(receiver);
+            //이메일 전송
+            emailService.sendTemplateMessage(uuid, toName, toEmail, fromEmail, templateDeadline); // 이메일로 템플릿 전송
+        }
         return null;
-//
-//		Templatefile templatefile = Templatefile.builder().
-//				templatefileOriginalName(fileName).
-//				templatefileSavedName(fromEmail + "/" + fileName).
-//				build();
-//		templateFileRepository.save(templatefile);
-
-//		// 템플릿 파일은 수신자 모두가 공유하고 사용자 위젯만 서로 다름
-//		for (int i = 0; i < documentTemplateSaveReqDTO.getTemplateName().length(); i++) { // 수신자들에게 템플릿 전송
-//			String uuid = UUIDGenerator.generateUUID(); // 템플릿 uuid
-//			String templateName = documentTemplateSaveReqDTO.getTemplateName(); // 템플릿 이름
-//			String templateDeadline = documentTemplateSaveReqDTO.getTemplateDeadline(); // 템플릿 마감일
-//			String toName = documentTemplateSaveReqDTO.getToName().get(i); // 수신자 이름
-//			String toEmail = documentTemplateSaveReqDTO.getToName().get(i); // 수신자 이메일
-//
-//			// 사용자 템플릿 생성
-//			Template template = Template.builder().
-//					member(member)
-//					.templatefileIdx(templatefile) // 템플릿 파일
-//					.templateUuid(uuid) // 템플릿 uuid
-//					.templateType(templateType) // 템플릿 종류
-//					.templateName(templateName) // 템플릿 파일 이름
-//					.templateIsFavorite(false) // 즐겨 찾기 안함
-//					.templateIsCompleted(false) // 작성 완료 안함
-//					.templateIsDeleted(false) // 삭제 안함
-//					.templateDeadline(templateDeadline) // 마감일
-//					.templateToName(toName) // 수신자 이름
-//					.templateToEmail(toEmail) // 수신자 이메일
-//					.templateFromName(member.getMemberName()).templateFromEmail(fromEmail).build();
-//			templateRepository.save(template);
-
-        // 사용자 위젯 생성
-//			List<TemplateWidgetDTO> templateWidget = documentTemplateSaveReqDTO.getTemplateWidget();
-//			List<TemplateWidget> templateWidgets = templateWidget.stream().map(widget ->
-//					TemplateWidget.builder()
-//							.templateX(widget.getX()) // 왼쪽위 x 좌표
-//							.templateDx(widget.getDx()) // 가로 길이
-//							.templateY(widget.getY()) // 왼쪽위 y 좌표
-//							.templateDy(widget.getDy()) // 세로 길이
-//							.templateType(widget.getType()) //  위젯 종류
-//							.templateInputText(widget.getInputText()) // 위젯 내용
-//							.templateUuid(uuid) // 템플릿 uuid
-//							.build())
-//					.collect(Collectors.toList());
-//			templateWidgetRepository.saveAll(templateWidgets);
-
-//			emailService.sendTemplateMessage(uuid, toName, toEmail, fromEmail, templateDeadline); // 이메일로 템플릿 전송
-//		}
-//		return null;
     }
 
     @Transactional
--- server/src/main/java/b209/docdoc/server/template/service/TemplateService.java
+++ server/src/main/java/b209/docdoc/server/template/service/TemplateService.java
@@ -1,11 +1,11 @@
 package b209.docdoc.server.template.service;
 
 import b209.docdoc.server.template.dto.Request.DocumentTemplateSaveReqDTO;
-<<<<<<< HEAD
+
 import b209.docdoc.server.template.dto.Response.TemplateNameResDTO;
-=======
+
 import b209.docdoc.server.template.dto.Response.TemplateResDTO;
->>>>>>> 80507c7015dbc8966053e15a2b6abf01c043cf54
+
 
 import java.util.List;
 
