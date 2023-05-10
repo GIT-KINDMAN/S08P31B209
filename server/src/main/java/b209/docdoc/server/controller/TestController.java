d8d28dee3af56223720f7026d064c5a7f6878169
이동훈 <mankind1990s@gmail.com>
2023-05-10 11:47:33 +0900

    chore: server 배포 #62
    Former-commit-id: 61cdc7438db63b393d21c1ab6af68e2c779ee1e4

--- server/src/main/java/b209/docdoc/server/controller/TestController.java
+++ server/src/main/java/b209/docdoc/server/controller/TestController.java
@@ -3,16 +3,11 @@ package b209.docdoc.server.controller;
 import b209.docdoc.server.config.security.auth.PrincipalDetails;
 import b209.docdoc.server.config.utils.Msg;
 import b209.docdoc.server.config.utils.ResponseDTO;
-import b209.docdoc.server.dto.member.SignModReqDTO;
-import b209.docdoc.server.dto.member.mod;
-import b209.docdoc.server.dto.member.modPw;
-import b209.docdoc.server.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
