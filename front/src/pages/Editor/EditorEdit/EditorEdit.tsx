4711d66fe6ec3d01bc64d03a7ac74121958e0d31
권동규 <ratsae0117@gmail.com>
2023-05-10 14:22:13 +0900

    feat: viewerTop Toolbar template #39

--- front/src/pages/Editor/EditorEdit/EditorEdit.tsx
+++ front/src/pages/Editor/EditorEdit/EditorEdit.tsx
@@ -6,28 +6,66 @@ const EditorEdit = () => {
 
   return (
     <>
-      <div tw="flex flex-row grow">
-        <div className="LeftSidebar" tw="min-w-[15rem] bg-blue-100">
-          left
-        </div>
-        <div className="CenterSidebar" tw="w-full">
-          <div tw="flex justify-center items-center bg-blue-900">
-            <div className="ZoomOut" tw="mx-2" onClick={() => dummyFunc()}>
-              <i className="fi fi-bs-zoom-out"></i>
-            </div>
-            <input
-              type="text"
-              value={"100%"}
-              tw="min-w-[4rem] max-w-[4rem] text-center bg-inherit"
-              onClick={() => dummyFunc()}
-            />
-            <div className="ZoomIn" tw="mx-2" onClick={() => dummyFunc()}>
-              <i className="fi fi-bs-zoom-in"></i>
+      <div className="viewer" tw="flex flex-col grow">
+        <div className="viewerTop">
+          <div
+            className="ViewerToolbarWrap"
+            tw="flex justify-center border-t-2 border-blue-700 py-1 bg-blue-800"
+          >
+            <div
+              className="ViewerToolbar"
+              tw="flex items-center text-sm text-lightgray-400 rounded-[2rem] bg-blue-600"
+            >
+              <div className="ZoomTool" tw="flex">
+                <div
+                  className="ZoomOut"
+                  tw="pt-1 mx-2"
+                  onClick={() => dummyFunc()}
+                >
+                  <i className="fi fi-bs-zoom-out"></i>
+                </div>
+                <input
+                  type="text"
+                  value={"100%"}
+                  tw="min-w-[4rem] max-w-[4rem] text-center bg-inherit"
+                  onClick={() => dummyFunc()}
+                />
+                <div
+                  className="ZoomIn"
+                  tw="pt-1 mx-2"
+                  onClick={() => dummyFunc()}
+                >
+                  <i className="fi fi-bs-zoom-in"></i>
+                </div>
+              </div>
+              <div className="HelpTool" tw="flex text-sm rounded-[2rem]">
+                <div
+                  className="ZoomIn"
+                  tw="pt-1 mx-2 text-base"
+                  onClick={() => dummyFunc()}
+                >
+                  <i className="fi fi-bs-info"></i>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div className="RightSidebar" tw="min-w-[15rem] bg-orange-100">
-          right
+        <div tw="flex flex-row grow">
+          <div
+            className="LeftSidebar"
+            tw="flex flex-col min-w-[15rem] max-w-[15rem] bg-orange-100"
+          >
+            left
+          </div>
+          <div className="Center" tw="w-full">
+            center view
+          </div>
+          <div
+            className="RightSidebar"
+            tw="flex flex-col min-w-[15rem] max-w-[15rem] bg-orange-100"
+          >
+            right
+          </div>
         </div>
       </div>
     </>
