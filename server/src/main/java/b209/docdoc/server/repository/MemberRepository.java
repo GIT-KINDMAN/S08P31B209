508194172bdf2ce59698c334bd26872446f5e52e
joykst96 <joykst96@gmail.com>
2023-05-10 10:48:20 +0900

    feat: add spring security filter chain & redis token management

---
+++ server/src/main/java/b209/docdoc/server/repository/MemberRepository.java
@@ -0,0 +1,20 @@
+package b209.docdoc.server.repository;
+
+import b209.docdoc.server.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Member findByMemberEmail(String memberEmail);
+
+    boolean existsByMemberEmail(String memberEmail);
+
+    @Query("update Member m set m.memberName = :name, m.memberPhone = :phone where m.memberIdx= :idx")
+    @Modifying(clearAutomatically = true)
+    int updateMember(@Param("name") String name, @Param("phone") String phone, @Param("idx") long idx);
+
+    @Query("select m.memberIdx from Member m where m.memberEmail like :memberEmail")
+    Long findIdxByMemberEmail(String memberEmail);
+}
