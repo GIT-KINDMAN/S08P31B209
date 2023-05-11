4e0dde6a2e19f56e2e19bdebdd5839144d668ce7
hunsang <gjstkd123@naver.com>
2023-05-11 15:02:58 +0900

    feat: Page3 분리

--- front/src/pages/Intro/Intro.tsx
+++ front/src/pages/Intro/Intro.tsx
@@ -2,9 +2,9 @@ import { Image, Label } from "@/components/atoms";
 import IntroNav from "@/pages/Intro/IntroNav/IntroNav";
 import IntroPage1 from "@/pages/Intro/IntroPage/IntroPage1";
 import IntroPage2 from "@/pages/Intro/IntroPage/IntroPage2";
+import IntroPage3 from "@/pages/Intro/IntroPage/IntroPage3";
 
 import Knock from "../../assets/Main/Knock.png";
-import MainImg2 from "../../assets/Main/MainImg2.jpg";
 
 import { useNavigate } from "react-router-dom";
 import tw from "twin.macro";
@@ -21,13 +21,7 @@ const Intro = () => {
       {/* 두번째 페이지 */}
       <IntroPage2 />
       {/* 캐러셀 영역*/}
-      <div>
-        <div>
-          <div> Intro-Page3-Text </div>
-          <div> Intro-Page3-carousel </div>
-          <div> Intro-Page3-Text </div>
-        </div>
-      </div>
+      <IntroPage3 />
       <div>
         <div>
           <div> Intro-Page4-Text </div>
