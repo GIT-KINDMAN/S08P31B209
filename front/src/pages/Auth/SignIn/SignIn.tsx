b699d9b90aa71c1a62e1490ccf08ebd1c6b91835
한재욱 <hju9707@naver.com>
2023-05-11 17:26:47 +0900

    feat: sign in api 구현 #87

--- front/src/pages/Auth/SignIn/SignIn.tsx
+++ front/src/pages/Auth/SignIn/SignIn.tsx
@@ -1,10 +1,36 @@
+import { loginAPI } from "@/api/api";
+
+import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import tw, { styled } from "twin.macro";
 
 const Login = () => {
   const navigate = useNavigate();
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const signIn = async (email: string, password: string) => {
+    try {
+      setIsLoading(true);
+      const response = await loginAPI.login(email, password);
+      setIsLoading(false);
+
+      if (response.status === 200) {
+        navigate("/");
+      } else {
+        alert("로그인에 실패했습니다.");
+      }
+    } catch (error) {
+      setIsLoading(false);
+      alert("로그인에 실패했습니다.");
+    }
+  };
+
   const login = () => {
-    return console.log("login");
+    if (emailRef.current && passwordRef.current) {
+      signIn(emailRef.current.value, passwordRef.current.value);
+    }
   };
 
   return (
@@ -24,7 +50,8 @@ const Login = () => {
           <button
             className="LoginBtn"
             tw="m-2 p-2 rounded-[0.5rem] bg-blue-400"
-            onClick={() => login}
+            onClick={login}
+            disabled={isLoading}
           >
             로그인
           </button>
