6adcfa89e1784e34682bcbc2de63608d3be91910
juseong <kjschocolate@gmail.com>
2023-05-11 14:09:08 +0900

    fix: 주소록 api 수정 완료

--- server/src/main/java/b209/docdoc/server/address/controller/AddressController.java
+++ server/src/main/java/b209/docdoc/server/address/controller/AddressController.java
@@ -6,9 +6,7 @@ import b209.docdoc.server.address.service.AddressService;
 import b209.docdoc.server.config.utils.Msg;
 import b209.docdoc.server.config.utils.ResponseDTO;
 import b209.docdoc.server.config.utils.SecurityManager;
-import b209.docdoc.server.template.dto.Request.DocumentTemplateSaveReqDTO;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,23 +20,21 @@ public class AddressController {
 
     @PostMapping("/save")
     public ResponseEntity<ResponseDTO> saveOneAddress(@RequestBody AddressRegisterReq req) {//@AuthenticationPrincipal PrincipalDetails principalDetails,
-        addressService.saveOneAddress(req, SecurityManager.getCurrentMember());
-        return ResponseEntity.ok().body(ResponseDTO.of(HttpStatus.OK, Msg.SUCCESS_MEMBER_MOD));
+        return ResponseEntity.ok().body(ResponseDTO.of(HttpStatus.OK, Msg.SUCCESS_ADDRESS_SAVE, addressService.saveOneAddress(req, SecurityManager.getCurrentMember())));
     }
 
     @GetMapping("/list")
     public ResponseEntity<ResponseDTO> getAddressList(@RequestParam("group") String group) {//@AuthenticationPrincipal PrincipalDetails principalDetails,
-        return ResponseEntity.ok().body(ResponseDTO.of(HttpStatus.OK, Msg.SUCCESS_MEMBER_MOD, addressService.getAddressListByGroup(group, SecurityManager.getCurrentMember())));
+        return ResponseEntity.ok().body(ResponseDTO.of(HttpStatus.OK, Msg.SUCCESS_ADDRESS_SEARCH, addressService.getAddressListByGroup(group, SecurityManager.getCurrentMember())));
     }
 
     @GetMapping("/list/editor")
     public ResponseEntity<ResponseDTO> getAddressListEditor(@RequestParam("name") String name) {//@AuthenticationPrincipal PrincipalDetails principalDetails,
-        return ResponseEntity.ok().body(ResponseDTO.of(HttpStatus.OK, Msg.SUCCESS_MEMBER_MOD, addressService.getAddressBoolListByName(name, SecurityManager.getCurrentMember())));
+        return ResponseEntity.ok().body(ResponseDTO.of(HttpStatus.OK, Msg.SUCCESS_ADDRESS_SEARCH, addressService.getAddressBoolListByName(name, SecurityManager.getCurrentMember())));
     }
 
     @PostMapping("/save/editor")
     public ResponseEntity<ResponseDTO> saveAddressListEditor(@RequestBody AddressEditorReq req) {//@AuthenticationPrincipal PrincipalDetails principalDetails,
-        addressService.saveAddressEditor(req, SecurityManager.getCurrentMember());
-        return ResponseEntity.ok().body(ResponseDTO.of(HttpStatus.OK, Msg.SUCCESS_MEMBER_MOD));
+        return ResponseEntity.ok().body(ResponseDTO.of(HttpStatus.OK, Msg.SUCCESS_ADDRESS_SAVE, addressService.saveAddressEditor(req, SecurityManager.getCurrentMember())));
     }
 }
