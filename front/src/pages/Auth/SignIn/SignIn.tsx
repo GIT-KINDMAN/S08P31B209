29d39e41b9ec4e800e688a17058ade780d52c3af
hunsang <gjstkd123@naver.com>
2023-05-11 16:48:47 +0900

    feat: signin atomic #86

--- front/src/pages/Auth/SignIn/SignIn.tsx
+++ front/src/pages/Auth/SignIn/SignIn.tsx
@@ -1,40 +1,67 @@
+import { Button, Label, TextInput } from "@/components/atoms";
+
+import { AuthAPI } from "../../../apis/index";
+
+// import SigninButton from "@/pages/Auth/AuthForm/SigninButton";
+// import SigninForm from "@/pages/Auth/AuthForm/SigninForm";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import tw, { styled } from "twin.macro";
+import tw from "twin.macro";
 
 const Login = () => {
   const navigate = useNavigate();
-  const login = () => {
-    return console.log("login");
-  };
+  // const login = () => {
+  //   return console.log("login");
+  // };
+
+  const [emailText, setEmailText] = useState("");
+  const [passwordText, setPasswordText] = useState("");
+  console.log(emailText, passwordText);
 
   return (
     <>
       <div tw="flex flex-col w-[30rem]">
         <div className="LoginForm" tw="flex flex-col">
           <div className="InputField" tw="flex flex-col">
-            <label> 이메일 </label>
-            <input type="email" tw="border-2"></input>
+            <Label text="이메일" fontSize="lg" />
+            <TextInput
+              type="email"
+              onChange={(e: any) => setEmailText(e.target.value)}
+            />
           </div>
           <div className="InputField" tw="flex flex-col">
-            <label> 비밀번호 </label>
-            <input type="password" tw="border-2"></input>
+            <Label text="비밀번호" fontSize="lg" />
+            <TextInput
+              type="password"
+              onChange={(e: any) => setPasswordText(e.target.value)}
+            />
           </div>
         </div>
-        <div className="ButtonWrap" tw="flex flex-col">
-          <button
-            className="LoginBtn"
-            tw="m-2 p-2 rounded-[0.5rem] bg-blue-400"
-            onClick={() => login}
+        {/* <SigninButton /> */}
+        <div className="ButtonWrap" tw="flex flex-col mx-auto">
+          <Button
+            className="LoginButton"
+            variant="primary"
+            custom={tw`w-80 mt-8 mb-2 p-2 rounded-[0.5rem] bg-blue-400 text-white`}
+            onClick={() => {
+              AuthAPI.login(emailText, passwordText)
+                .then((request) => {
+                  console.log("로그인 :", request.data);
+                })
+                .catch((e) => console.log(e));
+            }}
           >
             로그인
-          </button>
-          <button
-            className="SignUpBtn"
-            tw="m-2 p-2 rounded-[0.5rem] bg-gray-400"
+          </Button>
+          <Button
+            className="SignupButton"
+            variant="primary"
+            // isOutline={true}
+            custom={tw`w-80 my-2 p-2 rounded-[0.5rem] bg-white text-blue-400 border-2`}
             onClick={() => navigate("/auth/signup")}
           >
             회원가입
-          </button>
+          </Button>
         </div>
       </div>
     </>
