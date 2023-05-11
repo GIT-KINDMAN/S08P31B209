e1bf3d574d3fb7a7789b33b520e2db680b124cc5
권동규 <ratsae0117@gmail.com>
2023-05-11 13:33:50 +0900

    refactor: icon atom #68

--- front/src/components/atoms/Icon/Icon.styled.tsx
+++ front/src/components/atoms/Icon/Icon.styled.tsx
@@ -1,15 +1,16 @@
 import { getSize } from "@/constants";
 
-import { StyleProps } from "./Icon.types";
+import { SProps } from "./Icon.types";
 
 import "@flaticon/flaticon-uicons/css/all/all.css";
 import tw, { styled } from "twin.macro";
 
-export const StyledIcon = styled.i((props: StyleProps) => [
-  tw`inline-block text-center h-fit`,
+export const SIcon = styled.i((props: SProps) => [
+  tw`inline-block`,
+  tw`w-fit h-fit`,
+
   props.size && getSize[props.size],
-  props.iconColor,
   props.custom,
 ]);
 
-export default StyledIcon;
+export default SIcon;
