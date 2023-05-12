55cd4da24c6e5413767721588014e7e2a2641ec0
권동규 <ratsae0117@gmail.com>
2023-05-12 15:02:25 +0900

    feat: metaDocSlice를 store에 추가 #68

--- front/src/store/store.tsx
+++ front/src/store/store.tsx
@@ -1,23 +1,32 @@
+import metaDocReducer from "./slice/metaDocSlice";
+
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  metaDoc: metaDocReducer,
+});
 
 const persistConfig = {
   key: "root",
   storage,
 
   // // persist제외하고 싶은 부분
-  // blacklist: [],
-  // // 또는 persist를 적용하고 싶은 부분 따로 설정하지 않으면 모두다 저장된다.
-  // whitelist: [],
+  blacklist: [],
+
+  // 또는 persist를 적용하고 싶은 부분 따로 설정하지 않으면 모두다 저장된다.
+  whitelist: ["metaDoc"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: (defaultMiddleware) =>
+    defaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
