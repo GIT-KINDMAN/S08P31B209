c1baad7a04515d38a669d70a4bed2f14b8a29c3c
이동훈 <mankind1990s@gmail.com>
2023-05-13 22:33:31 +0900

    feat: docsfile save/load 기능 개발 완료 #84

--- server/src/main/java/b209/docdoc/server/docsfile/controller/DocsfileController.java
+++ server/src/main/java/b209/docdoc/server/docsfile/controller/DocsfileController.java
@@ -1,12 +1,10 @@
 package b209.docdoc.server.docsfile.controller;
 
-import b209.docdoc.server.box.controller.BoxController;
-import b209.docdoc.server.box.service.BoxService;
+import b209.docdoc.server.config.security.auth.MemberDTO;
 import b209.docdoc.server.config.utils.Msg;
 import b209.docdoc.server.config.utils.ResponseDTO;
 import b209.docdoc.server.docsfile.dto.request.DocsfileSaveReqDTO;
 import b209.docdoc.server.docsfile.service.DocsfileService;
-import b209.docdoc.server.entity.Docsfile;
 import b209.docdoc.server.file.dto.FileDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -16,9 +14,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import b209.docdoc.server.config.utils.SecurityManager;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -41,10 +39,10 @@ public class DocsfileController {
         return ResponseEntity.ok().body(ResponseDTO.of(HttpStatus.OK, Msg.SUCCESS_DOCS_SAVE, docsfileService.saveFile(file, docsfileSaveReqDTO)));
     }
 
-    @GetMapping("/{uuid}/{email}")
-    public ResponseEntity<?> getDocsFile(@PathVariable String uuid, @PathVariable String email) throws IOException {
-//    @GetMapping("/{uuid}")
-//    public ResponseEntity<?> getDocsFile(@PathVariable String uuid, @AuthenticationPrincipal String email) throws IOException {
+    @GetMapping("/{uuid}")
+    public ResponseEntity<?> getDocsFile(@PathVariable String uuid) throws IOException {
+        MemberDTO member = SecurityManager.getCurrentMember();
+        String email = member.getEmail();
         FileDTO fileDTO = docsfileService.getDocsfile(uuid, email);
         ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(Paths.get(fileDTO.getSavedPath())));
 
