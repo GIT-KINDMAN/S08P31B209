c16a2a170c8db33a70d987d7ed308181a1f226a3
juseong <kjschocolate@gmail.com>
2023-05-13 15:55:32 +0900

    merge
    Former-commit-id: fd969a4de27503ff1f1c1d183f75eb70983745f2 [formerly 6afc95f379f472041736e12519ba283c9ce7e49a]
    Former-commit-id: 2655e66d7a35e7aee49def6dbc457c1f7b5dc941

--- server/src/main/java/b209/docdoc/server/controller/TestController.java
+++ server/src/main/java/b209/docdoc/server/controller/TestController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(value = "/test")
 @RequiredArgsConstructor
+@CrossOrigin(origins = {"https://k8b209.p.ssafy.io", "http://localhost:5173"})
 public class TestController {
 
     private static final String METHOD_NAME = TestController.class.getName();
