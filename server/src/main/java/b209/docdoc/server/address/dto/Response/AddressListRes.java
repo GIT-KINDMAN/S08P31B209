86b9926fb1ad75ad666c448caa3bc69186662584
juseong <kjschocolate@gmail.com>
2023-05-15 16:38:27 +0900

    fix: 주소록 조회 api 수정

--- server/src/main/java/b209/docdoc/server/address/dto/Response/AddressListRes.java
+++ server/src/main/java/b209/docdoc/server/address/dto/Response/AddressListRes.java
@@ -10,11 +10,23 @@ import java.util.List;
 @Setter
 public class AddressListRes {
     @JsonProperty(access = JsonProperty.Access.READ_WRITE)
+    List<String> groups;
+
+    @JsonProperty(access = JsonProperty.Access.READ_WRITE)
     List<AddressInfo> addresses;
 
-    public static AddressListRes of(List<AddressInfo> list) {
+    public static AddressListRes of(List<AddressInfo> addresses) {
         AddressListRes res = new AddressListRes();
-        res.setAddresses(list);
+        res.setAddresses(addresses);
+
+        return  res;
+    }
+
+    public static AddressListRes of(List<String> groups, List<AddressInfo> addresses) {
+        AddressListRes res = new AddressListRes();
+        groups.add(0, "전체그룹");
+        res.setGroups(groups);
+        res.setAddresses(addresses);
 
         return  res;
     }
